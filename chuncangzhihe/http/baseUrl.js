7615b271f5bfa0b957016d6ae97e84df1836e6b5
hujunhao <hongpao@2dfire.com>
2019-02-11 17:15:50 +0800

    11

--- chuncangzhihe/http/baseUrl.js
+++ chuncangzhihe/http/baseUrl.js
@@ -4,8 +4,8 @@
 export const appId = ''
 
 const BASE_URL_PUBLISH = {
-    // API_BASE_URL: 'http://10.1.135.16:3001',
-    API_BASE_URL: 'http://localhost:3001'
+    API_BASE_URL: 'http://10.1.135.16:3001',
+    // API_BASE_URL: 'http://localhost:3001'
 };
 
 export default BASE_URL_PUBLISH
