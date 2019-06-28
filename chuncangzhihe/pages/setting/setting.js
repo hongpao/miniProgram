c76896e90491e01485f712e41badd1aeaf41a92b
hujunhao <hongpao@2dfire.com>
2019-06-28 17:57:27 +0800

    首页

--- chuncangzhihe/pages/setting/setting.js
+++ chuncangzhihe/pages/setting/setting.js
@@ -13,8 +13,8 @@ Page({
         phase: '',
         date: '',
         week: '',
-        ff: [false, false, false, false, false],
-        lf: [false, false]
+        // ff: [false, false, false, false, false],
+        // lf: [false, false]
     },
     onLoad() {},
     onShow() {
@@ -40,33 +40,43 @@ Page({
     },
 
     bindKeyInput(e) {
-        let {
-            detail,
-            target
-        } = e
-        let number = target.dataset.number || ''
-        let value = detail.value
-        console.log('ee', number, value)
+        // let {
+        //     detail,
+        //     target
+        // } = e
+        // let number = target.dataset.number || ''
+        // let value = detail.value
+        // console.log('ee', number, value)
 
-        let ff = this.data.ff
-        for (let index in ff) {
-        }
-        console.log(ff)
-        this.setData({
-            ff
-        })
+        // let newFF = []
+        // for (let i = 0; i < 5; i++) {
+        //     newFF.push(i - 1 == parseInt(number))
+        // }
+
+        // this.setData({
+        //     ff: newFF
+        // })
     },
 
-    //新增数据
-    addNew() {
-        console.log('新增数据')
-    },
+    // //新增数据
+    // addNew() {
+    //     console.log('新增数据')
+    // },
 
     bindDateChange(e) {
         const val = e.detail.value
         if (!MiniUtils.Utils.isEmpty(val)) {
             this.settingDate(val)
         }
+    },
+
+    //保存
+    save() {
+        wx.showToast({
+            title: '保存成功',
+            duration: 2000
+        })
+
     },
 
     bindWeekChange(e) {
