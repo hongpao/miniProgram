8ab3644412c55b5ab519857549c0ee0c556f6bbc
hujunhao <hongpao@2dfire.com>
2019-12-24 16:45:53 +0800

    页面

--- chuncangzhihe/pages/mahjong/mahjong.js
+++ chuncangzhihe/pages/mahjong/mahjong.js
@@ -5,7 +5,24 @@ Page({
      * 页面的初始数据
      */
     data: {
-        isStart: false //是否已经开桌
+        isStart: false, //是否已经开桌
+        basePrise: '',
+        user: [{
+            name: '红袍',
+            turn: []
+        }, {
+            name: '红袍2',
+            turn: []
+        }, {
+            name: '红袍3',
+            turn: []
+        }, {
+            name: '红袍4',
+            turn: []
+        }, {
+            name: '台板',
+            turn: []
+        }]
     },
 
     /**
@@ -22,8 +39,42 @@ Page({
 
     },
 
+    //改变输入框的值
+    changeVal(e) {
+        let type = e.target.dataset.type || 1
+        let value = e.detail.value || ''
+        let {
+            basePrise,
+            user
+        } = this.data
+
+        switch (+type) {
+            case 1:
+                basePrise = value.trim()
+                break
+            case 2:
+                user[0].name = value.trim()
+                break
+            case 3:
+                user[1].name = value.trim()
+                break
+            case 4:
+                user[2].name = value.trim()
+                break
+            case 5:
+                user[3].name = value.trim()
+                break
+        }
+
+        this.setData({
+            basePrise,
+            user
+        })
+    },
+
     //开桌
     toStart() {
+
         this.setData({
             isStart: true
         })
